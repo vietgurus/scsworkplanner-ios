a448214309aa536e6e7d29bbcc21c6897f2527a1
yohei-sasaki <yohei-sasaki@kke.co.jp>
2016-11-18 16:38:04 +0800

    second view half way

---
+++ WorkPlanner/CreateNewToDoCardController.swift
@@ -0,0 +1,9 @@
+//
+//  CreateNewToDoCardController.swift
+//  WorkPlanner
+//
+//  Created by YOHEISASAKI on 17/11/16.
+//  Copyright © 2016 YOHEISASAKI. All rights reserved.
+//
+
+import Foundation
