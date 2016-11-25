7e9590076db4ada34ae8a2c92ea697638b5bedf9
yohei-sasaki <yohei-sasaki@kke.co.jp>
2016-11-25 09:47:20 +0800

    implemented carousel

---
+++ WorkPlanner/ToDoCardCell.swift
@@ -0,0 +1,9 @@
+//
+//  ToDoCardCell.swift
+//  WorkPlanner
+//
+//  Created by YOHEISASAKI on 24/11/16.
+//  Copyright © 2016 YOHEISASAKI. All rights reserved.
+//
+
+import Foundation
---
+++ WorkPlanner/ToDoCardListView.swift
@@ -0,0 +1,9 @@
+//
+//  ToDoCardListView.swift
+//  WorkPlanner
+//
+//  Created by YOHEISASAKI on 23/11/16.
+//  Copyright © 2016 YOHEISASAKI. All rights reserved.
+//
+
+import Foundation
